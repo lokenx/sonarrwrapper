afbfbf2ffb214ae9754e2058d819f68cd220aa2f
loken <mikhail@delport.ca>
2016-01-17 20:22:21 -0500

    Series stats

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lokenx:sonarrwrapper',
-  version: '0.0.4',
+  version: '0.0.7',
   // Brief, one-line summary of the package.
   summary: 'Wrapper for the Sonarr API',
   // URL to the Git repository containing the source code for this package.
